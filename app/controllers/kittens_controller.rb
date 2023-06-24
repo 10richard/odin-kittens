55e445280cb0143e09c34b9045de0d6352079484
Richard Ten <rten04@icloud.com>
2023-06-24 13:33:12 -0400

    Add all RESTful actions and fill them out

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,46 @@
 class KittensController < ApplicationController
+
+    def index
+        @kittens = Kitten.all
+    end
+
+    def new
+        @kitten = Kitten.new
+    end
+
+    def create
+        @kitten = Kitten.new(kitten_params)
+
+        if @kitten.save
+            redirect_to @kitten and return
+        end
+        render :new, status: :unprocessable_entity
+    end
+
+    def show
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def edit
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def update
+        @kitten = Kitten.find(params[:id])
+
+        if @kitten.update(kitten_params)
+            redirect_to @kitten and return
+        end
+        render :edit, status: :unprocessable_entity
+    end
+
+    def destroy
+        @kitten = Kitten.find(params[:id])
+        @kitten.destroy
+    end
+
+    private
+    def kitten_params
+        params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
 end
